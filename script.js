1076eff538d58432b598a283df28e6d864f22681
ormle <101660382+ormle@users.noreply.github.com>
2024-01-17 12:33:20 -0700

    Accordion Test

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.onload = function() {
-    var button = document.querySelector('button');
-    button.addEventListener('click', function() {
-      alert("Pressed!!");
-    }); 
+/*Accordion buttons */
+const accordion = document.getElementsByClassName('container');
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function() {
+    this.classList.toggle('active')
+   })
 }
