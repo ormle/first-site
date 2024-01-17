82e903137818591de0e69b99d989e112d35c98ec
ormle <101660382+ormle@users.noreply.github.com>
2024-01-17 12:56:42 -0700

    attempt fix accordion?

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*Accordion buttons */
 const accordion = document.getElementsByClassName('container');
+
 for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function() {
+  accordion[i].addEventListener('click', function () {
     this.classList.toggle('active')
-   })
+  })
 }
