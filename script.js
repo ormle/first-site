f1e761fdfdb55a784845f7f4376e4e601f16c237
ormle <101660382+ormle@users.noreply.github.com>
2024-01-17 16:14:18 -0700

    fix accordion click

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
   /* Accordion buttons */
-  const labels = document.querySelectorAll('.accordion .label');
+  const labels = document.querySelectorAll('.accordion .container .label');
 
   labels.forEach(function (label) {
     label.addEventListener('click', function () {
       // Toggle the 'active' class for the corresponding container
-      const container = this.nextElementSibling; // Assuming the container follows the label
+      const container = this.parentElement; // Assuming the container follows the label
       container.classList.toggle('active');
     });
   });
