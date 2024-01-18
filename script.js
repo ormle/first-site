d4fab1011db85ebc93fecdde9b199ed0a9c79730
ormle <101660382+ormle@users.noreply.github.com>
2024-01-17 21:48:17 -0700

    About me section

--- script.js
+++ script.js
@@ -7,34 +7,11 @@ window.onload = function () {
       // Toggle the 'active' class for the corresponding container
       const container = this.parentElement; // Assuming the container follows the label
       container.classList.toggle('active');
+      console.log("Hello!");
     });
   });
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('.sidebar a');
-
-    links.forEach(function (link) {
-      link.addEventListener('click', function (event) {
-        //Prevent default anchor behavior
-        event.preventDefault();
-        //Remove active class from other link
-        links.forEach(function (otherLink) {
-          otherLink.classList.remove('active');
-      });
-
-      //Make clicked one active
-      link.classList.add('active');
-
-      //Scroll to content
-      const targetId = link.getAttribute('href').split('#')[1];
-      const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth'});
-      }
-    });
-  });
-});
 };
+
 
 
